35772557a8da0bc42bfa219b59f1462d21cc373b
Risto Vatsar <r.vatsar@gmail.com>
2023-12-10 18:15:46 +0200

    dotenv added

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const { Pool } = require("pg");
+// const itemsPool = new Pool({
+//     user: "postgres",
+//     password: "admin1234",
+//     host: "localhost",
+//     port: 5432,
+//     database: "myrecipes",
+// });
+
 const itemsPool = new Pool({
-    user: "postgres",
-    password: "admin1234",
-    host: "localhost",
-    port: 5432,
-    database: "myrecipes",
+    connectionString: process.env.DBConnectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 module.exports = itemsPool;
