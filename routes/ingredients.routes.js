56bca3769a84130b51a5f832147685bb2b0263dd
Risto Vatsar <r.vatsar@gmail.com>
2023-12-11 16:46:43 +0200

    ingredients POST and DELETE added

--- routes/ingredients.routes.js
+++ routes/ingredients.routes.js
@@ -11,4 +11,58 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.post("/", async (req, res) => {
+    const { ingredientname } = req.body;
+    console.log(ingredientname);
+
+    const data = await db.query(
+        "SELECT * FROM ingredient WHERE ingredientname = $1;",
+        [ingredientname]
+    );
+    console.log(data.rows);
+
+    if (data.rows.length !== 0) {
+        res.json({ message: "Ingredient already exists." });
+    } else {
+        try {
+            const result = await db.query(
+                "INSERT INTO ingredient (ingredientname) VALUES ($1);",
+                [ingredientname]
+            );
+
+            console.log(result.rowCount);
+            res.status(200).json({
+                message: `${result.rowCount} rows have been added.`,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+});
+
+router.delete("/", async (req, res) => {
+    const { ingredientname } = req.body;
+
+    const data = await db.query(
+        "SELECT * FROM ingredient WHERE ingredientname = $1;",
+        [ingredientname]
+    );
+
+    if (data.rows.length === 0) {
+        res.json({ message: "There is no such ingredient!" });
+    } else {
+        try {
+            const result = await db.query(
+                "DELETE FROM ingredient WHERE ingredientname = $1;",
+                [ingredientname]
+            );
+            res.status(200).json({
+                message: `${result.rowCount} rows have been deleted.`,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+});
+
 module.exports = router;
